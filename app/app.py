363a7771c2e1751d03a2755e224076eaa771b88a
muzammil35 <145228299+muzammil35@users.noreply.github.com>
2024-03-30 19:10:29 -0600

    Update app.py

--- app/app.py
+++ app/app.py
@@ -131,5 +131,4 @@ def create_playlist():
     return jsonify({'success': True}), 200
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=5000)
